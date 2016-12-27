12a7592e12d67b2dc3fc7b4c411819337369ba88
YuanJunYao <785570262@qq.com>
2016-12-27 17:18:38 +0800

    Restruct the project.
    Consider which version styles and which elements should be add to version.h and write down it at the README file.

--- src/version.sh
+++ src/version.sh
@@ -1,18 +1,25 @@
-version_full=`git describe --dirty`
+# version
 version=`git describe`
+version_full=`git describe --dirty`
 version_simple=`git describe --abbrev=0`
+
+echo "version:"
+echo $version
+echo $version_full
+echo $version_simple
+
+#element
+tag=$version_simple
+id=`echo $version | sed -e "s/^${tag}-\([0-9]+\)-.*"`
 dirty=`echo $version_full | sed -e "s/.*\(dirty\)/\1/g"`
+
+echo "element"
+echo $tag
+echo $id
+echo $dirty
 
 # commit=`echo 12 | sed -e "s/-[0-9]+-[0-9a-z]\{8}$//g"`
 commit=`echo $version | sed -e "s/-[0-9]+/###/g"`
-
-
-echo "full version is $version_full"
-echo "version is $version"
-echo "simple version is $version_simple"
-echo "version is $dirty"
-echo "commit id is $commit"
-
 if [ `echo $temp_version | grep -e "-dirty$" -c` -ne 0 ]; then
 	is_dirty=1
 	echo "is dirty"
